3cc9df853938f4781fc6c7f97b2b5155c04f5995
RijaRiaz <60516095+RijaRiaz@users.noreply.github.com>
2020-03-01 00:57:58 +0500

    Marathon Race

--- main.cpp
+++ main.cpp
@@ -1,52 +1,40 @@
 #include <stdio.h>
-#include<iostream>
+#include <iostream>
 using namespace std;
-void getdata();
-void avghigh();
-void avglow();
-string days [12]={"January","February","March","April","May","June","July","August","September","October","November","December"};
-string temp[2]={"Highest","Lowest"};   
-int arr[2][12];
+void names();
+void totavg();
 int main()
 {
-    getdata();
-    avghigh();
-    avglow();
+    names();
+ 
 }
-void getdata(){
-    for(int i=0;i<=1;i++){
-        cout<<"Enter the "<<temp[i]<<" temperature"<<endl;
-        for(int j=0;j<=11;j++){
-         cout<<days[j]<<" : ";
+void names()
+{
+   for(int i=0;i<=6;i++){
+        cout<<"          "<<"Day "<<i+1;
+   }
+    cout<<endl;
+    string nam[5]={"Jason", "Ravi", "Sheila", "Ankit", "Samantha"};
+    for(int i=0;i<=4;i++){
+        cout<<nam[i]<<endl;
+        cout<<endl;
+   }
+   int sum ;
+   float avg;
+   int arr[5][7];
+   for(int i=0;i<=4;i++){
+         sum=0;
+       cout<<"Enter your scores "<<nam[i]<<endl;
+       for(int j=0;j<=6;j++){
+           cout<<"Day "<<j+1<<": ";
            cin>>arr[i][j];
-        }   
-    cout<<endl; 
+           sum=sum+arr[i][j];
+       }
+       avg=sum/7.0;
+       cout<<"Sum is "<<sum<<endl;
+       cout<<"average is "<<avg<<endl;
+   }
+   cout<<endl;
+   
     }
-}
-int sum;
-double avgh;
-void avghigh(){
-      
-          sum=0;
-           for(int j=0;j<=11;j++){
-            sum=sum+arr[0][j];
-           }
-           avgh=sum/12.0;
-           cout<<"Average high temperature of the year is "<<avgh<<endl;
-    }
-
-double avgl;
-void avglow(){
-      
-          sum=0;
-           for(int j=0;j<=11;j++){
-            sum=sum+arr[1][j];
-           }
-           avgh=sum/12.0;
-           cout<<"Average low temperature of the year is "<<avgh<<endl;
-    }
-    
-       
-    
-
 
